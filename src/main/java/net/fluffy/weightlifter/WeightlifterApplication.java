8ebd8a6a79aa10f3bd69cd55c2730ee7317172c7
Fluffy <fluffywin@gmail.com>
2024-10-12 14:42:47 -0400

    starting api project

---
+++ src/main/java/net/fluffy/weightlifter/WeightlifterApplication.java
@@ -0,0 +1,13 @@
+package net.fluffy.weightlifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightlifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightlifterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fluffy/weightlifter/WeightlifterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fluffy.weightlifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightlifterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
